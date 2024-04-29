a5b940dd8625a0e524932d4adb6005a6f1b879aa
Thunderbqlt <samw3363@gmail.com>
2024-04-30 09:17:34 +1200

    imported the php mailer code library

--- send-email.php
+++ send-email.php
@@ -4,3 +4,8 @@ $name = $_POST["name"];
 $email = $_POST["email"];
 $subject = $_POST["subject"];
 $message = $_POST["message"];
+
+require "vendor/autoload.php";
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;

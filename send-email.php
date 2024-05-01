5c94901aa4668d57d670f113b7123b44545ef392
Thunderbqlt <samw3363@gmail.com>
2024-05-01 22:45:29 +1200

    First iteration completed

--- send-email.php
+++ send-email.php
@@ -9,3 +9,33 @@ require "vendor/autoload.php";
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
+
+$mail = new PHPMailer(true);
+
+$mail->isSMTP();
+$mail->SMTPAuth = true;
+
+$mail->Host = "smtp.gmail.com";
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+$mail->Port = 587;
+$mail->SMTPOptions = array(
+    'ssl' => array(
+        'verify_peer' => false,
+        'verify_peer_name' => false,
+        'allow_self_signed' => true
+    )
+    );
+
+$mail->Username = "pursuingdiscipline@gmail.com";
+$mail->Password = "qxth myqj etpx aqei";
+
+$mail->setFrom($email, $name);
+$mail->addAddress("s.h.worth@student.saintkentigern.com", "User");
+
+$mail->Subject = $subject;
+$mail->Body = $message;
+
+$mail->send();
+
+echo "email sent";  
+
